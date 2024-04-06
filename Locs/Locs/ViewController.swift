7b4a23be603111bf001b7b2a744924bcb9f7a6e4
BunChan <stephenjamesfitzgerald@yahoo.com>
2024-04-06 13:08:44 +0900

    prj setup

---
+++ Locs/Locs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Locs
+//
+//  Created by STEPHEN FITZGERALD on 2024/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
