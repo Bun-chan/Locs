57d477b2a4baaba27a6d7bd7836225c9b7b2e38b
BunChan <stephenjamesfitzgerald@yahoo.com>
2024-04-06 14:50:20 +0900

    added model vars

--- Locs/Locs/Location/LocationModel.swift
+++ Locs/Locs/Location/LocationModel.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 import CoreLocation
+import UIKit
 
 class Location {
-    var location: CLLocation
+    var location: CLLocation //lat, long.
+    var name: String?
+    var group: String? //Group like locs together. RecordStore group, izakaya group, yakitori group. This will be used when fetching from CoreData.
+    var images: [UIImage]?
+    var description: String? //Enter a description about the loc.
     
     init(location: CLLocation) {
         self.location = location
